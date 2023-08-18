c47c7d6de7d7c0df2f63e194f4bb1611d4e2f034
kylekovac <kylekovac97@outlook.com>
2023-08-18 17:53:40 +1000

    renderHosingt

--- server/database/mongoDB.js
+++ server/database/mongoDB.js
@@ -1,5 +1,5 @@
 require("dotenv").config()
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGO_DATABASE_CONNECTION_STRING ) 
 let userCollection;
