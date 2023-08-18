a78d105a2d06722cd9476fbd4bf440e654b233e0
kylekovac <kylekovac97@outlook.com>
2023-08-18 22:15:23 +1000

    render deployed but not loading fix

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", 
+    origin: "http://localhost:4000", 
   }
 });
 const fileUpload = require("express-fileupload");
